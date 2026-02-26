5fdb052eb8cef3293f7cc9e35e56f7052d8c973e
Demydov Maksym <maksimdemidov05@gmail.com>
2026-02-26 16:00:49 +0200

    Created models for User and Category

---
+++ FanPulseApi/FanPulseApi/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FanPulseApi.Models;
+
+public sealed class Category
+{
+    public Guid Id { get; set; }
+    
+    public Guid? ParentId { get; set; }
+    
+    public Category? Parent { get; set; }
+    
+    public required string Name { get; set; }
+
+    public ICollection<Category> Children { get; set; } = new List<Category>();
+    
+    public ICollection<User> Users { get; set; } = new List<User>(); 
+
+}
