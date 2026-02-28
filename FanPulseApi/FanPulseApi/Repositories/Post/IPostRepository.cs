465be95847e5c7d9625c09a0cf3c9c2d0e0733fc
Nizar <nairp6502@gmail.com>
2026-02-28 15:21:49 +0200

    Added Bacis interface

---
+++ FanPulseApi/FanPulseApi/Repositories/Post/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+
+namespace FanPulseApi.Models
+{
+    public interface IPostRepository
+    {
+        public Task<List<Post>> GetPosts(int startFrom, int count);
+
+        public Task<Post> GetPostById(Guid id);
+
+        public Task<bool> DeletePost(Guid id);
+
+        public Task<Post> UpdatePost(Guid postId,Post post);
+
+
+      
+    }
+}
