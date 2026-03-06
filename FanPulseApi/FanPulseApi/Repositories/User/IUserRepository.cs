4d5bc691ace98cd853883117ba5b6eef422c0d17
Demydov Maksym <maksimdemidov05@gmail.com>
2026-03-06 16:27:27 +0200

    Added repositories for models Category and User

---
+++ FanPulseApi/FanPulseApi/Repositories/User/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace FanPulseApi.Repositories.User;
+
+public interface IUserRepository
+{
+    Task<Models.User?> GetUserByIdAsync(Guid userId);
+
+    Task<List<Models.User>> GetAllUsersAsync();
+    
+    Task<Models.User> CreateUserAsync(Models.User user);
+    
+    public Task<bool> DeleteUserAsync(Guid id);
+    
+    public Task<bool> UpdateUserAsync(Models.User user);
+    
+    Task<Models.User?> GetUserByEmailAsync(string email);
+
+}
