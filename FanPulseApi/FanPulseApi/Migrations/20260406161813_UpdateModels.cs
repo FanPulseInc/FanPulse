cb30b6b17db277911ec858b404b95faab16c968b
Leonardo Kapone <nairp6502@gmail.com>
2026-04-06 19:18:58 +0300

    Added migration file

---
+++ FanPulseApi/FanPulseApi/Migrations/20260406161813_UpdateModels.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FanPulseApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "PostLikes",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "PostLikes",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CommentId",
+                table: "PostLikes",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes");
+
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "PostLikes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "PostLikes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "PostLikes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
