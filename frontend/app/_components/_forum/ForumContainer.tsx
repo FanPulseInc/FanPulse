2d8a3182a86fe929fa198f4fc2a4c5c922329b6a
Demydov Maksym <maksimdemidov05@gmail.com>
2026-04-11 03:16:40 +0300

    Updated Forum page and Thread page, almost done, but lines that fixing replies should be fixed!

--- frontend/app/_components/_forum/ForumContainer.tsx
+++ frontend/app/_components/_forum/ForumContainer.tsx
@@ -1,6 +1,6 @@
 export const ForumContainer = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="w-full bg-[#f8f8f8] py-10 min-h-screen">
+        <div className="w-full bg-[#E6E6E6] py-10 min-h-screen">
             <div className="max-w-[1039px] mx-auto px-4">
                 {children}
             </div>
